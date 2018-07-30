ac0451af768873ed7c226670089d06377aa8890a
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-07-30 16:42:06 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,9 +12,10 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(username: params["username"], password: params["password"])
-    binding.pry
+
     if @user
       session[:user_id] = @user.id
+      binding.pry
       redirect_to '/account'
     else
       halt 302
