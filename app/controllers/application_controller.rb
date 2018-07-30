6f88bca2694128e5cf0d53cf46c0e5ccae277c2e
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-07-30 16:43:08 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,6 @@ class ApplicationController < Sinatra::Base
       session[:user_id] = @user.id
       redirect_to '/account'
     else
-      halt 302
       redirect_to '/account'
     end
   end
